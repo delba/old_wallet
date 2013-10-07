b4ac3a8b89fd6ed640ce2459a9efde4ec3a27144
delba <delba@server.fake>
2013-10-07 21:49:54 +0200

    client method

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -1,15 +1,13 @@
 class StripeController < ApplicationController
   STRIPE_CONNECT_URL = 'https://connect.stripe.com'
 
-  before_action :set_client
-
   def authorize
     connect_params = {
       scope: 'read_write',
       redirect_uri: callback_url
     }
 
-    redirect_to @client.auth_code.authorize_url(connect_params)
+    redirect_to client.auth_code.authorize_url(connect_params)
   end
 
   def callback
@@ -22,8 +20,8 @@ class StripeController < ApplicationController
 
 private
 
-  def set_client
-    @client ||= OAuth2::Client.new(
+  def client
+    OAuth2::Client.new(
       ENV['STRIPE_CLIENT_ID'],
       ENV['STRIPE_SECRET_KEY'],
       site: STRIPE_CONNECT_URL
@@ -31,7 +29,7 @@ private
   end
 
   def auth_token
-    @auth_token ||= @client.auth_code.get_token(params[:code])
+    @auth_token ||= client.auth_code.get_token(params[:code])
   end
 
   def stripe_attributes
