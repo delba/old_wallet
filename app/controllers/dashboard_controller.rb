c79c5ab9d3a4233558e022742924a3d9c97fb6cf
delba <delba@server.fake>
2013-10-07 20:12:07 +0200

    connect to Stripe

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,35 @@
 class DashboardController < ApplicationController
+  STRIPE_CONNECT_URL = 'https://connect.stripe.com'
+
+  before_action :set_client, only: [:connect, :callback]
+
   def index
+  end
+
+  def connect
+    connect_params = {
+      scope: 'read_write',
+      redirect_uri: callback_url
+    }
+
+    redirect_to @client.auth_code.authorize_url(connect_params)
+  end
+
+  def callback
+    if params[:code]
+      @auth_token = @client.auth_code.get_token(params[:code])
+    end
+
+    redirect_to root_url
+  end
+
+private
+
+  def set_client
+    @client ||= OAuth2::Client.new(
+      ENV['STRIPE_CLIENT_ID'],
+      ENV['STRIPE_SECRET_KEY'],
+      site: STRIPE_CONNECT_URL
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Wallet::Application.routes.draw do
   root 'dashboard#index'
+
+  controller :dashboard do
+    get 'connect'
+    get 'callback'
+  end
 end
