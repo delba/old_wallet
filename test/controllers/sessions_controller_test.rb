64922b29b9d50b84283d44373e652094dc8c9b6a
delba <delba@server.fake>
2013-10-09 19:54:35 +0200

    controller tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "delete destroy" do
+    session[:user_id] = 42
+
+    delete :destroy
+
+    assert_nil session[:user_id]
+    assert_redirected_to root_url
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @jane = users(:jane)
+  end
+
+  test "get :show" do
+    get :show, id: @jane
+
+    assert_equal @jane, assigns(:user)
+    assert_template :show
+  end
 end
