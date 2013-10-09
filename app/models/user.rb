807665289f930967a7cd53db8f73dd7f4b26cac5
delba <delba@server.fake>
2013-10-09 19:10:56 +0200

    serialize currencies_supported

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  serialize :currencies_supported
+
   def self.stripe_connect(stripe_attributes)
     create_with(stripe_attributes).
     find_or_create_by(stripe_user_id: stripe_attributes[:stripe_user_id])
