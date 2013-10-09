9e727b80488c1d5254da9210f2a83e4a8e524536
delba <delba@server.fake>
2013-10-09 19:11:24 +0200

    fetch stripe account on connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ActiveRecord::Base
   serialize :currencies_supported
 
   def self.stripe_connect(stripe_attributes)
-    create_with(stripe_attributes).
-    find_or_create_by(stripe_user_id: stripe_attributes[:stripe_user_id])
+    user = find_by(stripe_user_id: stripe_attributes[:stripe_user_id])
+
+    unless user
+      user = new(stripe_attributes)
+      account = Stripe::Account.retrieve(user.stripe_token)
+      user.default_currency = account.default_currency
+      user.currencies_supported = account.currencies_supported
+      user.save
+    end
+
+    user
   end
 end
